919de366a24ce4b29adebbba88cb0350be3ac16c
amit thakur <amithakur081@gmail.com>
2022-01-08 14:00:30 +0530

    refactor

--- controller/auth.js
+++ controller/auth.js
@@ -5,7 +5,7 @@ const jwt = require('jsonwebtoken');
 
 
 const generateToken = (payload)=>{
-    return jwt.sign(payload , process.env.JWT_SECRET);
+    return jwt.sign(payload , process.env.JWT_SECRET ,{expiresIn :"7d"});
 
 }
 
